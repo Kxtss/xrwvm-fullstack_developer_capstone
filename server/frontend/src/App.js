ffa30004829b2935c4e30e0a5c5e774d2c3badcc
kxtss <marcofcu213@gmail.com>
2025-05-28 22:13:48 -0400

    Add Docker and Docker Compose configuration for application containerization

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,6 @@
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register";
-import Dealers from "./components/Dealers/Dealers"
+import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer";
 import PostReview from "./components/Dealers/PostReview";
 import { Routes, Route } from "react-router-dom";
